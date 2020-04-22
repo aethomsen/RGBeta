5b0f58d35b0917af1cd9655ec023fa234dac8c2b
Anders Eller Thomsen <mr.a.eller@gmail.com>
2020-04-22 10:35:45 +0200

    Minor fixes.

--- Betas.m
+++ Betas.m
@@ -1,5 +1,5 @@
 (*
-	Author: Anders Eller Thomsen 
+	Author: Anders Eller Thomsen
 	Released under the MIT license (see 'MIT_license.txt').
 *)
 Begin["Betas`"]
@@ -10,29 +10,29 @@ Begin["Betas`"]
 (*Symmetrizing in indices*)
 Sym[i1_, i2_][expr_] := expr /2 + ReplaceAll[expr, {i1 -> i2, i2 -> i1}] /2 ;
 AntiSym[i1_, i2_][expr_] := expr /2 - ReplaceAll[expr, {i1 -> i2, i2 -> i1}] /2 ;
-Sym[a_, b_, c_, d_][expr_] := 
+Sym[a_, b_, c_, d_][expr_] :=
 	Block[{perm},
 		perm = {a -> #[[1]], b -> #[[2]], c -> #[[3]], d -> #[[4]]} & /@ Permutations[{a, b, c, d}];
-		Mean[expr/.perm]	
+		Mean[expr/.perm]
 	];
-Sym4[a_, b_, c_, d_][expr_] := 
+Sym4[a_, b_, c_, d_][expr_] :=
 	Block[{perm},
 		perm = {a -> #, # -> a} & /@ {a, b, c, d};
-		Mean[expr/.perm]	
-	];	
-Sym[a_, b_, c_][expr_] := 
+		Mean[expr/.perm]
+	];
+Sym[a_, b_, c_][expr_] :=
 	Block[{perm},
 		perm = {a -> #[[1]], b -> #[[2]], c -> #[[3]]} & /@ Permutations[{a, b, c}];
-		Mean[expr/.perm]	
+		Mean[expr/.perm]
 	];
-AntiSym[indices__][expr_] := 
+AntiSym[indices__][expr_] :=
 	Block[{indList, subs, s},
 		indList = List[indices];
 		subs = MapThread[Rule, {indList, #}] & /@ Permutations @ indList;
 		Signature[indList] Sum[Signature[s[[;;, 2]] ] expr /.s, {s, subs}] / Factorial @Length @ indList
 	];
 
-CanonizeMatrices[expr_] := ReplaceAll[Matrix[y__][h1_[i1_], h2_[i2_]] /; !OrderedQ[{i1, i2}] :> 
+CanonizeMatrices[expr_] := ReplaceAll[Matrix[y__][h1_[i1_], h2_[i2_]] /; !OrderedQ[{i1, i2}] :>
 	Matrix[Sequence @@ Reverse[Trans /@ List@ y]][h1 @ i2, h2 @ i1] ] @ expr;
 
 (*Functions that speed up evaluation by applying expand succesively to each couple of terms in the evaluation.*)
@@ -57,74 +57,74 @@ BetaTerm[coupling_, loop_Integer] :=
 		(*Determines the correct tensor structure based on coupling type*)
 		Switch[$couplings @ coupling
 		,x_ /; MemberQ[Keys @ $gaugeGroups, x],
-			If[loop > 3, 
+			If[loop > 3,
 				Message[BetaTerm::loopNumber, "gauge", 3];
 				Return @ $Failed;
 			];
 			group = $couplings @ coupling;
 			beta = Ttimes[G2Matrix[C1, $A] ,GaugeTensors[loop], G2Matrix[$B, C2], $gaugeGroups[group, Projector][C1, C2] ];
-			
+
 			(*Puts U1-mixing matrix on matrix form*)
 			If[MatchQ[$gaugeGroups[group, LieGroup], U1[n_] /; n >1],
 				beta = beta /. Matrix[l1_List][group[adj] @ v1] Matrix[l2_List][group[adj] @ v2] :> Outer[Times, l1, l2];
 			];
 		,Yukawa,
-			If[loop > 2, 
+			If[loop > 2,
 				Message[BetaTerm::loopNumber, "Yukawa", 2];
 				Return @ $Failed;
 			];
-			
+
 			Switch[$yukawas[coupling, Chirality]
 			,Left,
 				tensor = YukawaTensors[loop][[1, 1]];
 			,Right,
 				tensor = YukawaTensors[loop][[2, 2]];
 			];
-			
+
 			beta = tensor $yukawas[coupling, Projector][$a, $i, $j] // Expand // Expand;
 		,Quartic,
-			If[loop > 2, 
+			If[loop > 2,
 				Message[BetaTerm::loopNumber, "quartic", 2];
 				Return @ $Failed;
 			];
-			
+
 			beta = QuarticTensors[loop] $quartics[coupling, Projector][$a, $b, $c, $d] // Expand // Expand;
 		,FermionMass,
-			If[loop > 2, 
+			If[loop > 2,
 				Message[BetaTerm::loopNumber, "fermion mass", 2];
 				Return @ $Failed;
 			];
-			
+
 			Switch[$fermionMasses[coupling, Chirality]
 			,Left,
 				tensor = FermionMassTensors[loop][[1, 1]];
 			,Right,
 				tensor = FermionMassTensors[loop][[2, 2]];
 			];
-			
+
 			beta = tensor $fermionMasses[coupling, Projector][$a, $i, $j] // Expand // Expand;
 		,Trilinear,
-			If[loop > 2, 
+			If[loop > 2,
 				Message[BetaTerm::loopNumber, "trilinear scalar", 2];
 				Return @ $Failed;
 			];
-			
+
 			beta = ScalarMassiveTensors[loop] $trilinears[coupling, Projector][$a, $b, $c, $d] // Expand // Expand;
 		,ScalarMass,
-			If[loop > 2, 
+			If[loop > 2,
 				Message[BetaTerm::loopNumber, "scalar mass", 2];
 				Return @ $Failed;
 			];
 			If[loop === 0,
 				Return @ 0;
 			];
-			
+
 			beta = ScalarMassiveTensors[loop] $scalarMasses[coupling, Projector][$a, $b, $c, $d] // Expand // Expand;
 		,_Missing,
 			Message[BetaTerm::unkown, coupling];
 			Return @ $Failed;
 		];
-		
+
 		(*Canonically order coupling indices if any*)
 		CanonizeMatrices @ RefineGroupStructures @ beta
 	];
@@ -136,12 +136,12 @@ BetaFunction::loopNumber = "The `1` beta function is only implemented up to `2` 
 BetaFunction[coupling_Symbol, loop_Integer, OptionsPattern[] ] ? OptionsCheck :=
 	Block[{coef = 4 Pi, firstTerm = 0, l},
 		Switch[$couplings @ coupling
-		,gr_ /; MemberQ[Keys @ $gaugeGroups, gr],	
+		,gr_ /; MemberQ[Keys @ $gaugeGroups, gr],
 			If[loop > 3,
 				Message[BetaFunction::loopNumber, "gauge", 3];
 				Return @ $Failed;
 			];
-		,Yukawa|Quartic|ScalarMass|Trilinear|FermionMass,	
+		,Yukawa|Quartic|ScalarMass|Trilinear|FermionMass,
 			If[loop > 2,
 				Message[BetaFunction::loopNumber, $couplings @ coupling, 2];
 				Return @ $Failed;
@@ -150,10 +150,10 @@ BetaFunction[coupling_Symbol, loop_Integer, OptionsPattern[] ] ? OptionsCheck :=
 			Message[BetaFunction::unkown, coupling];
 			Return @ $Failed;
 		];
-		
+
 		If[OptionValue @ RescaledCouplings, coef = 1; ];
 		If[OptionValue @ FourDimensions, firstTerm = 1; ];
-		
+
 		Sum[ Power[coef, -2 l] BetaTerm[coupling, l], {l, firstTerm, loop}]
 	];
 
@@ -166,10 +166,10 @@ QuarticBetaFunctions[loop_Integer, opt:OptionsPattern[]] ? OptionsCheck :=
 			Message[QuarticBetaFunctions::loopNumber];
 			Return @ $Failed;
 		];
-		
+
 		couplings = Keys @ $quartics;
 		Print["The quartic couplings are ", couplings];
-		
+
 		(*Finds inversion matrix for the quartic projectors*)
 		qProjections = CheckProjection /@ couplings;
 		invMatrix = Transpose @ Table[Simplify @ D[qProjections, c], {c, couplings}];
@@ -178,25 +178,28 @@ QuarticBetaFunctions[loop_Integer, opt:OptionsPattern[]] ? OptionsCheck :=
 			Return @ $Failed;
 		];
 		invMatrix = Inverse @ invMatrix;
-		
+
 		(*Extracts beta functions*)
 		betaFunctions = Monitor[
 							Table[BetaFunction[c, loop, opt], {c, couplings}]
 						,StringForm["Evaluating the `` \[Beta]-function", c] ];
-		
+
 		invMatrix . betaFunctions // Expand
 	];
 
 
-(*Function for finalizing a betafunction, bringing it from a nice compact output to a form more suitable for 
+(*Function for finalizing a betafunction, bringing it from a nice compact output to a form more suitable for
 further Mathematica manipulations. Can also be used to specify particular cases for coupling matrices.*)
-Finalize[expr_, OptionsPattern[{Parameterizations -> {}}] ] :=
+Options[Finalize] = {Parameterizations -> {}, BarToConjugate -> False};
+Finalize[expr_, opt:OptionsPattern[]] ? OptionsCheck :=
 	Internal`InheritedBlock[{out, Bar, Trans, Matrix},
 		out = CanonizeMatrices @ expr /. OptionValue @ Parameterizations;
 			Bar[a_List] := Bar /@ a;
-			Bar[Times[a__]] := Bar /@ Times[a];
-			Bar[Plus[a__]] := Bar /@ Plus[a];
-			Bar[a_Symbol] := Conjugate @ a;
+			Bar[Times[a_, b__]] := Bar /@ Times[a, b];
+			Bar[Plus[a_, b__]] := Bar /@ Plus[a, b];
+			If[OptionValue @ BarToConjugate,
+				Bar[a_Symbol] := Conjugate @ a;
+			];
 			Bar[a_] /; NumberQ[a] := Conjugate @ a;
 			Trans[a_List] /; MatrixQ[a] := Transpose @ a;
 			Trans[a_List] /; VectorQ[a] := a;
@@ -211,7 +214,7 @@ CheckProjection[coupling_Symbol] :=
 		Switch[$couplings @ coupling
 		,x_ /; MemberQ[Keys @ $gaugeGroups, x],
 			Ttimes[G2Matrix[A, B], $gaugeGroups[$couplings @ coupling, Projector][B, A] ] // Expand
-		,Yukawa,			
+		,Yukawa,
 			Switch[$yukawas[coupling, Chirality]
 			,Left,
 				tensor = Yuk[a, i, j][[1, 1]];
--- RGBeta.m
+++ RGBeta.m
@@ -1,11 +1,11 @@
 (*
 ################################################################
-This is RGBeta, Version 0.3. 
+This is RGBeta, Version 0.3.
 Last modefied 03-04-2020
 
 Released under the MIT license (see 'MIT_license.txt').
 
-Author: Anders Eller Thomsen 
+Author: Anders Eller Thomsen
 ################################################################
 *)
 
@@ -13,11 +13,11 @@ BeginPackage["RGBeta`"]
 
 (*################################################################*)
 (*----------Defines symbols and functions used by RGBeta----------*)
-(*################################################################*)  
-$a::usage = $b::usage = $c::usage = $d::usage = $i::usage = $j::usage = $A::usage = $B::usage = $vev::usage =  $vevSelect::usage =  
+(*################################################################*)
+$a::usage = $b::usage = $c::usage = $d::usage = $i::usage = $j::usage = $A::usage = $B::usage = $vev::usage =  $vevSelect::usage =
 	"Used as a global dummy index."
 
-$couplings::usage = 
+$couplings::usage =
 	"$coupling is an association containing the types of all the couplings that have been defined in the model."
 
 $fermionMasses::usage =
@@ -26,16 +26,16 @@ $fermionMasses::usage =
 $fermions::usage =
 	"$fermions is an association containing all the internal information on the fermions that have been declared in the model."
 
-$gaugeCoefficients::usage = 
+$gaugeCoefficients::usage =
 	"$gaugeCoefficients is an internal replacement list containing the coefficient of all tensor constractions used in the quartic beta function."
 
 $gaugeGroups::usage =
 	"$gaugeGroups is an association containing all the internal information on the gauge groups that have been declared in the model."
 
-$quarticCoefficients::usage = 
+$quarticCoefficients::usage =
 	"$quarticCoefficients is an internal replacement list containing the coefficient of all tensor constractions used in the quartic beta function."
 
-$quartics::usage = 
+$quartics::usage =
 	"$quartics is an association containing all internal information on the quartic couplings."
 
 $scalarMasses::usage =
@@ -45,12 +45,12 @@ $scalars::usage =
 	"$scalars is an association containing all the internal information on the scalars that have been declared in the model."
 
 $trilinears::usage =
-	"$trilinears is an association contraining all the internal informaiton on the trilinear scalar couplings." 
+	"$trilinears is an association contraining all the internal informaiton on the trilinear scalar couplings."
 
-$yukawaCoefficients::usage = 
+$yukawaCoefficients::usage =
 	"$yukawaCoefficients is an internal replacement list containing the coefficient of all tensor constractions used in the yukawa beta function."
 
-$yukawas::usage = 
+$yukawas::usage =
 	"$yukawas is an association containing all internal information on the yukawa couplings."
 
 $RGBetaVersion::usage =
@@ -60,44 +60,44 @@ adj::usage = fund::usage = S2::usage = A2::usage =
 	"Representation name defined defined by the Define(SO/Sp/SU)Group functions."
 
 f1::usage = f2::usage = s1::usage = s2::usage = s3::usage = s4::usage = v1::usage = v2::usage =
-	"Index name used for flavor indices on kinetic-mixing, Yukawa, or quartic couplings."  
+	"Index name used for flavor indices on kinetic-mixing, Yukawa, or quartic couplings."
 
-del::usage = 
+del::usage =
 	"del[rep, a, b] represents a Kronecker delta in \"rep space\" with indices a and b."
 
-delA2::usage = 
+delA2::usage =
 	"delA2[group, a, i, j] represents the singlet contraction between the A2 index a and the two fundamental indices i and j."
 
-delIndex::usage = 
+delIndex::usage =
 	"del[rep, a, b] represents a Kronecker delta function in \"rep space\" with either a or b being an Integer fixing the value of the other summation index."
 
-delS2::usage = 
+delS2::usage =
 	"delS2[group, a, i, j] represents the singlet contraction between the S2 index a and the two fundamental indices i and j."
 
-eps::usage = 
+eps::usage =
 	"eps[rep, a, b] represents the 2-index antisymmetric tensor in \"rep space\" with indices a and b."
 
-fStruct::usage = 
+fStruct::usage =
 	"fStruct[group, A, B, C] represents the structure constant of the group with indices A, B, and C."
 
 lcSymb::usage =
 	"Levi-Civita symbol"
 
-sDelF::usage = 
+sDelF::usage =
 	"sDelF[field, generalIndex, specificIndex] represents the structure delta for a fermionic field."
 
-sDelS::usage = 
+sDelS::usage =
 	"sDelS[field, generalIndex, specificIndex] represents the structure delta for a scalar field."
 
-sDelV::usage = 
+sDelV::usage =
 	"sDelV[field, generalIndex, specificIndex] represents the structure delta for a vector field."
 
 tGen::usage =
 	"tGen[rep, A, a, b] represents a group generator of the representation \"rep\" with adjoint index A. a and b are the two indices of rep. "
 
-Chirality::usage = Coupling::usage = CouplingBar::usage = CouplingIndices::usage = CouplingMatrix::usage = FermionMass::usage = Field::usage = 
-Fields::usage = FlavorIndices::usage = FourDimensions::usage = GaugeRep::usage = GroupInvariant::usage = Indices::usage = Invariant::usage = LieGroup::usage = Mass::usage= 
-MassIndices::usage = Parameterizations::usage = Projector::usage = Quartic::usage = RescaledCouplings::usage = ScalarMass::usage = SelfConjugate::usage = Trilinear::usage = UniqueArrangements::ussage = Yukawa::usage = 
+Chirality::usage = Coupling::usage = CouplingBar::usage = CouplingIndices::usage = CouplingMatrix::usage = FermionMass::usage = Field::usage =
+Fields::usage = FlavorIndices::usage = FourDimensions::usage = GaugeRep::usage = GroupInvariant::usage = Indices::usage = Invariant::usage = LieGroup::usage = Mass::usage=
+MassIndices::usage = Parameterizations::usage = Projector::usage = Quartic::usage = RescaledCouplings::usage = ScalarMass::usage = SelfConjugate::usage = Trilinear::usage = UniqueArrangements::ussage = Yukawa::usage =
 	"Function option and/or key used in global association lists of fields and/or couplings."
 
 SO::usage = Sp::usage = SU::usage = U1::usage =
@@ -112,25 +112,25 @@ AddFermionMass::ussage =
 AddGaugeGroup::usage =
 	"AddGaugeGroup[coupling, groupName, lieGroup[n]] is a function used to define a gauge group in the model."
 
-AddQuartic::usage = 
+AddQuartic::usage =
 	"AddQuartic[coupling, {scal1, scal2, scal3, scal4}] defines a quartic interaction between 4 scalar fields."
-	
+
 AddScalar::usage =
 	"AddScalar[field] is a function used to define a scalar field in the model."
 
 AddScalarMass::ussage =
 	"AddScalarMass[mass, {scal1, scal2}] defines a mass term between the two scalar fields."
 
-AddTrilinear::usage = 
+AddTrilinear::usage =
 	"AddTrilinear[coupling, {scal1, scal2, scal3}] defines a Yukawa interaction between a scalar and two fermion fields."
 
-AddYukawa::usage = 
+AddYukawa::usage =
 	"AddYukawa[coupling, {scal, ferm1, ferm2}] defines a Yukawa interaction between a scalar and two fermion fields."
 
 AddVector::usage =
 	"AddVector[field, group] is an internal function used to define a vector field of a gauge group."
 
-AntiSym::usage = 
+AntiSym::usage =
 	"AntiSym[a, b][expr] is an internal function for antisymmetrizing expr in a and b."
 
 AveragePermutations::usage =
@@ -145,40 +145,40 @@ BetaTerm::usage =
 BetaFunction::usage =
 	"BetaFunction[coupling, loop] computes the entire beta function of the coupling to the given loop order."
 
-Bcoef::usage = 
+Bcoef::usage =
 	"Bcoef[i, j, k] is used internally to denote the coefficients of the differnet tensor contraction appering in the beta functions."
 
-Casimir2::usage = 
+Casimir2::usage =
 	"Casimir2[rep] sets the quadratic casimir of a given representation."
 
 CheckProjection::usage =
 	"CheckProjection[coupling] returns the result of the automatic projection operator of the coupling on the corresponding generalized coupling."
 
-CouplingPermutations::usage = 
+CouplingPermutations::usage =
 	"CouplingPermutations[fields, invariant] provides at list of all unique permutations of a coupling of the fields (List) and with the given group invariance function."
 
-DefineLieGroup::usage = 
+DefineLieGroup::usage =
 	"DefineLieGroup[groupName, lieGroup[n]] sets groupName to be a lie group of type SU(n), SO(n), Sp(n), or U(1)^n and defines invariants for several common representations of said group."
 
 Dim::usage =
 	"Dim[rep] sets the dimension of a given representation."
 
-FermionMassTensors::usage = 
+FermionMassTensors::usage =
 	"FermionMassTensors[loop] is a function that computes all the tensor contractions used the general fermion mass beta function at the given loop order."
 
-Finalize::usage = 
+Finalize::usage =
 	"Finalize[expr] performs additional refinement of beta function expressions. Only works for expressions with up to 2 nontrivial flavor indices."
 
-FGauge::usage = 
+FGauge::usage =
 	"FGauge[A, B, C] is a function that generates the general gauge structure constants."
 
-G2Matrix::usage = 
+G2Matrix::usage =
 	"G2Matrix[a, b] is a function that generates the general gauge coupling matrix."
 
-GaugeTensors::usage = 
+GaugeTensors::usage =
 	"GaugeTensors[loop] is a function that computes all the tensor contractions used the general gauge beta function at the given loop order."
 
-Lam::usage = 
+Lam::usage =
 	"Lam[a, b, c, d] is a function that generates the general quartic coupling."
 
 Matrix::usage =
@@ -190,7 +190,7 @@ OptionsCheck::usage =
 QuarticBetaFunctions::usage =
 	"QuarticBetaFunctions[loop] returns all quartic beta functions to the given loop order using diagonalized projectors."
 
-QuarticTensors::usage = 
+QuarticTensors::usage =
 	"QuarticTensors[loop] is a function that computes all the tensor contractions used the general quartic beta function at the given loop order."
 
 RemoveField::usage =
@@ -199,13 +199,13 @@ RemoveField::usage =
 RemoveInteraction::usage =
 	"RemoveCoupling[coupling, ...] is a function that removes one or more couplings and corresponding interactions/gauge groups from the model."
 
-ResetModel::usage = 
+ResetModel::usage =
 	"ResetModel[] resets all tensors and removes all fields and coupling definitions made in the current instance of RGBeta."
 
-RefineGroupStructures::usage = 
+RefineGroupStructures::usage =
 	"RefineGroupStructures[expr] decomposes generators of non-fundamental representations of the groups to the fundamental ones, whereby identities can be applied."
 
-ReInitializeSymbols::usage = 
+ReInitializeSymbols::usage =
 	"ReInitializeSymbols[] is a function which when called flushes all previous definitions for symbol behaviour under implicit summation."
 
 RepresentationCheck::usage =
@@ -214,7 +214,7 @@ RepresentationCheck::usage =
 ScalarMassiveTensors::usage =
 	"ScalarMassiveTensors[loop] is a function that computes all the tensor contractions used in the the trillinear and scalar mass beta functions at the given loop order."
 
-SetReal::usage = 
+SetReal::usage =
 	"SetReal[x1,...] makes x1,... behave as real parameters under complex conjugation (the Bar function)."
 
 Sym::usage =
@@ -226,43 +226,43 @@ Sym4::usage =
 Tensor::usage =
 	"Tensor[x][i,...] represents the tensor x with open indices i,..."
 
-Tferm::usage = 
+Tferm::usage =
 	"Tferm[A, i, j] is a function that generates the general gauge generator matrix for the fermions."
 
-TfermTil::usage = 
+TfermTil::usage =
 	"TfermTil[A, i, j] is a function that generates the general gauge generator matrix for the fermions with an applied tilde."
 
-TraceNormalization::usage = 
+TraceNormalization::usage =
 	"TraceNormalization[rep] sets the trace normalization of a given representation."
 
-Trans::usage = 
+Trans::usage =
 	"Trans[coupling] represents the transposed quantity of a coupling with two indices."
 
-Tscal::usage = 
+Tscal::usage =
 	"Tscal[A, i, j] is a function that generates the general gauge generator matrix for the scalars."
 
-Tdot::usage = 
+Tdot::usage =
 	"Tdot[a, b,...] is an internal function the sequantially expands the matrix product of all the arguments."
 
-Ttimes::usage = 
+Ttimes::usage =
 	"Ttimes[a, b,...] is an internal function the sequantially expands the product of all the arguments."
 
-Yuk::usage = 
+Yuk::usage =
 	"Yuk[a, i, j] is a function that generates the general yukawa coupling."
 
-YukawaTensors::usage = 
+YukawaTensors::usage =
 	"YukawaTensors[loop] is a function that computes all the tensor contractions used the general yukawa beta function at the given loop order."
 
-YukTil::usage = 
+YukTil::usage =
 	"YukTil[a, i, j] is a function that generates the general yukawa coupling with an applied tilde."
 
 (*##############################################*)
 (*---------------Loads components---------------*)
-(*##############################################*) 
-Begin["`Private`"] (* Begin Private Context *) 
+(*##############################################*)
+Begin["`Private`"] (* Begin Private Context *)
 	$RGBetaVersion = "RGBeta v0.3";
 	Print[$RGBetaVersion, " by Anders Eller Thomsen"];
-	
+
 	(*Loads package files*)
 	dir = DirectoryName @ $InputFileName;
 	Block[{$Path = {dir}},
@@ -271,21 +271,21 @@ Begin["`Private`"] (* Begin Private Context *)
 		<< GroupsAndIndices`;
 		<< Tensors`;
 	];
-	
+
 	(*Initializes dummy index notation and global model information*)
 	ResetModel[];
 
-	(*Protects global symbols*)	
+	(*Protects global symbols*)
 	Protect[$a, $b, $c, $d, $i, $j, $A, $B];
 	Protect[SO, Sp, SU, U1];
 	Protect[f1, f2, s1, s2, s3, s4];
-	
+
 End[] (* End Private Context *)
 
 
 (*###########################################*)
 (*---------------Options check---------------*)
-(*###########################################*) 
+(*###########################################*)
 General::invalidopt = "Option `1` for function `2` received invalid value `3`.";
 General::optexpectsval = "Option `1` for function `2` received invalid value `3`. A `4` is expected.";
 OptionMessage[opt_, func_, val_] := Message[General::invalidopt, opt, func, val];
@@ -295,9 +295,10 @@ OptionMessage[FlavorIndices, func_, val_] := Message[General::optexpectsval, Fla
 OptionMessage[GaugeRep, func_, val_] := Message[General::optexpectsval, GaugeRep, func, val, List];
 OptionMessage[GroupInvariant, func_, val_] := Message[General::optexpectsval, GroupInvariant, func, val, Function];
 OptionMessage[MassIndices, func_, val_] := Message[General::optexpectsval, MassIndices, func, val, Function];
-OptionMessage[Parameterizations, func_, val_] := Message[General::optexpectsval, Parameterizations, func, val, "List of rules"];
+OptionMessage[Parameterizations, func_, val_] := Message[General::optexpectsval, Parameterizations, func, val, "List of replacement rules"];
 
 (*Tests for specific options. Form expected is OptionTest[function, options] *)
+OptionTest[_, BarToConjugate] = BooleanQ;
 OptionTest[_, Chirality] = MatchQ[Left|Right];
 OptionTest[_, CouplingIndices] = MatchQ[_Function];
 OptionTest[_, CouplingMatrix] = MatchQ[Automatic|_List];
@@ -306,12 +307,12 @@ OptionTest[_, FourDimensions] = BooleanQ;
 OptionTest[_, GaugeRep] = MatchQ[_List];
 OptionTest[_, GroupInvariant] = MatchQ[_Function];
 OptionTest[_, MassIndices] = MatchQ[_Function];
-OptionTest[_, Parameterizations] = MatchQ[_Function];
+OptionTest[_, Parameterizations] = MatchQ[_List];
 OptionTest[_, RescaledCouplings] = BooleanQ;
 OptionTest[_, SelfConjugate] = BooleanQ;
 
 Attributes @ OptionsCheck = {HoldFirst};
-OptionsCheck @ func_[___, opts : OptionsPattern[]] := 
+OptionsCheck @ func_[___, opts : OptionsPattern[]] :=
 	And @@ (OptionTest[func, #1][#2] || OptionMessage[#1, func, #2] &) @@@ FilterRules[List[opts], Options @ func];
 
 
